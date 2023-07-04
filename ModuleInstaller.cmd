3d4934523f149a2154eedc43bad1ac497487f0c7
amiho <123155117+amiho-dev@users.noreply.github.com>
2023-07-04 12:37:57 +0200

    Updated ModuleInstaller.cmd

--- ModuleInstaller.cmd
+++ ModuleInstaller.cmd
@@ -1,22 +1,29 @@
-@echo off	
+@echo off
+
 echo Welcome to the SCR-Autopilot module installer!
 echo WARNING: Python 3.9.6 MUST be installed!
 pause
+
 echo Please wait...
 echo Waiting for 10 seconds for the processes to start...
-echo Ignore the "Deleted File" popup, your temporary folder is being cleaned up for installation.
-timeout 10	
-	del /q /f /s %temp%
-	del /q /f /s temp
-	cd ..		
-	cd ..	
-	cd ..	
-	cd ..
+timeout 10
+
+echo Cleaning up temporary files...
+del /q /f /s %temp%
+cd ..		
+cd ..	
+cd ..	
+cd ..
+
 echo __________________________________________________________________________________________
-echo If this is failing, please (re-) install Python 3.9.6.
+echo If the installation fails, please consider (re-) installing Python 3.9.6.
 pause
+
+echo Installing required Python packages...
 py -m pip install --upgrade pip
 py -m pip install opencv_python requests PyDirectInput ahk Flask numpy pytesseract Pillow pywin32
+
 echo __________________________________________________________________________________________
+echo Module installation completed.
 echo You may now close this window.
 pause
