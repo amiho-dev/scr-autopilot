35b06c62a0a0e537441f429ce06b1f41a96b6e50
amiho <123155117+amiho-dev@users.noreply.github.com>
2023-07-05 21:01:24 +0200

    Update ModuleInstaller.cmd

--- ModuleInstaller.cmd
+++ ModuleInstaller.cmd
@@ -6,7 +6,7 @@ pause
 
 echo Please wait...
 echo Waiting for 10 seconds for the processes to start...
-timeout 10
+timeout 10 >nul
 
 echo Cleaning up temporary files...
 del /q /f /s %temp%
@@ -15,15 +15,22 @@ cd ..
 cd ..	
 cd ..
 
-echo __________________________________________________________________________________________
-echo If the installation fails, please consider (re-) installing Python 3.9.6.
-pause
+echo *************************************************************************************************
+echo (!) If the installation fails, please consider (re-) installing Python 3.9.6.
+echo (!) If you have Python 3.9.6 installed, press Y. If not, press N.
+choice /c YN /n /m "Your choice: "
+if errorlevel 2 (
+        echo Please install Python 3.9.6 and execute the installer.cmd file again.
+        pause
+        exit /b
+    )
+)
 
 echo Installing required Python packages...
 py -m pip install --upgrade pip
 py -m pip install opencv_python requests PyDirectInput ahk Flask numpy pytesseract Pillow pywin32
 
-echo __________________________________________________________________________________________
+echo *************************************************************************************************
 echo Module installation completed.
 echo You may now close this window.
 pause
